46a1edf0cb2c63d45eefe46e70ba436cb31832a3
hikaru <n19004@std.it-college.ac.jp>
2019-12-20 16:10:40 +0900

    テンプレート

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from 'react'
+
+const Greeting = props => {}
 
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
-  render () {
-    return ()
-      <div />}
 
+  render () {
+    return (
+      <div>
+        <Greeting />
+      </div>
+    )
+  }
 }
 
 export default App
